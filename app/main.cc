e68d3f28b87682ebf05095272a21d4ee4bd13618
matin-jalali <matinjalalitekiyeh@gmail.com>
2024-06-25 15:08:51 -0400

    message >> / << for read and write

--- app/main.cc
+++ app/main.cc
@@ -1,60 +1,118 @@
-#include <iostream>
+//#include <iostream>
 
-#ifdef _WIN32
-#define _WIN32_WINNT 0X0A00
-#endif
+////#ifdef _WIN32
+////#define _WIN32_WINNT 0X0A00
+////#endif
 
-#define ASIO_STANDLONE
-#include <boost/asio.hpp>
-#include <boost/asio/ts/buffer.hpp>
-#include <boost/asio/ts/internet.hpp>
-#include <thread>
+////#define ASIO_STANDLONE
+////#include <boost/asio.hpp>
+////#include <boost/asio/ts/buffer.hpp>
+////#include <boost/asio/ts/internet.hpp>
+////#include <thread>
 
-int main(int argc, char **argv)
-{
+//int main(int argc, char **argv)
+//{
 
-    boost::system::error_code ec;
+//    boost::system::error_code ec;
 
-    // create a "context" - essentially the platform spec interface.
-    boost::asio::io_context context;
+//    // create a "context" - essentially the platform spec interface.
+//    boost::asio::io_context context;
 
-    // get the address of somewhere we wish to connect to
-    boost::asio::ip::tcp::endpoint endpoint(boost::asio::ip::make_address("51.38.81.49", ec), 80);
+//    // get the address of somewhere we wish to connect to
+//    boost::asio::ip::tcp::endpoint endpoint(boost::asio::ip::make_address("51.38.81.49", ec), 80);
 
-    // create socket, the context will deliver the impl
-    boost::asio::ip::tcp::socket socket(context);
+//    // create socket, the context will deliver the impl
+//    boost::asio::ip::tcp::socket socket(context);
 
-    socket.connect(endpoint, ec);
+//    socket.connect(endpoint, ec);
 
-    if (!ec) {
-        std::cout << "Connected!" << std::endl;
-    }else{
-        std::cout << "Failed to connect to address\n" << ec.message() << std::endl;
-    }
+//    if (!ec) {
+//        std::cout << "Connected!" << std::endl;
+//    }else{
+//        std::cout << "Failed to connect to address\n" << ec.message() << std::endl;
+//    }
 
-    if (socket.is_open()) {
+//    if (socket.is_open()) {
 
-        std::string sRequest =
-                "Get /index.html HTTP/1.1\r\n"
-                "Host: example.com\r\n"
-                "Connection: close\r\n\r\n";
+//        std::string sRequest =
+//                "Get /index.html HTTP/1.1\r\n"
+//                "Host: example.com\r\n"
+//                "Connection: close\r\n\r\n";
 
-        socket.write_some(boost::asio::buffer(sRequest.data(), sRequest.size()), ec);
+//        socket.write_some(boost::asio::buffer(sRequest.data(), sRequest.size()), ec);
 
-        using namespace std::chrono_literals;
-        std::this_thread::sleep_for(5s);
+//        using namespace std::chrono_literals;
+//        std::this_thread::sleep_for(5s);
 
-        size_t bytes = socket.available();
-        std::cout << "Bytes available: " << bytes << std::endl;
+//        size_t bytes = socket.available();
+//        std::cout << "Bytes available: " << bytes << std::endl;
 
-        if (bytes > 0) {
-            std::vector<char> vBuffer(bytes);
-            socket.read_some(boost::asio::buffer(vBuffer.data(), vBuffer.size()), ec);
+//        if (bytes > 0) {
+//            std::vector<char> vBuffer(bytes);
+//            socket.read_some(boost::asio::buffer(vBuffer.data(), vBuffer.size()), ec);
 
-            for (auto c : vBuffer)
-                std::cout << c;
-        }
-    }
+//            for (auto c : vBuffer)
+//                std::cout << c;
+//        }
+//    }
 
-    return 0;
+//    return 0;
+//}
+
+#include "message.h"
+
+enum class Custom_msg_type : uint32_t {
+    FireBullet,
+    MovePlayer
+};
+
+int main() {
+
+    magno::net::mpi::message<Custom_msg_type> msg;
+    msg.header.id = Custom_msg_type::FireBullet;
+
+
+    int a = 1;
+    bool b = true;
+    float c = 3.14159f;
+
+    struct {
+        float x,y;
+    } d[5];
+
+    msg << a << b << c << d;
+
+    a = 99;
+    b = false;
+    c = 99.0f;
+
+
+    msg >> d >> c >> b >> a;
+
+
+
+    std::cout << "salam\n";
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/message.h
+++ app/message.h
@@ -3,83 +3,52 @@
 
 #include "net_common.h"
 
-namespace magno::mpi {
 
-
-template <typename T>
+namespace magno::net::mpi {
+template<typename T>
 struct message_header {
     T id {};
     uint32_t size = 0;
 };
 
-template <typename T>
+template<typename T>
 struct message {
     message_header<T> header;
     std::vector<uint8_t> body;
 
-    //!
-    //! \brief size return size of entire message packet in bytes.
-    //! \return size of entire message packet in bytes.
-    //!
     size_t size() const {
         return sizeof (message_header<T>) + body.size();
     }
 
-    //!
-    //! \brief operator << override for std::cout compatiblity - produces friendly description message.
-    //!
-    friend std::ostream& operator<<(std::ostream& os, const message<T>& msg) {
-        return os << "ID: " << int(msg.header.id) << " Size: " << msg.header.size;
+    friend std::ostream& operator << (std::ostream& os, const message<T>& msg) {
+        os << "ID: " << int(msg.header.id) << " size: " << msg.header.size;
+        return os;
     }
 
-    //!
-    //! \brief pushes any pod data into the message buffer.
-    //!
-    template <typename DataType>
-    friend message<T>& operator<<(message<T>& msg, const DataType& data) {
-
-        static_assert (std::is_standard_layout<DataType>::value, "Data is too complex - not standard layout.");
-        static_assert (std::is_trivial<DataType>::value, "Data is not trivial.");
-
-        // cache current size of vector, as this will be the point we insert the data.
+    template<typename DataType>
+    friend message<T>& operator << (message<T>& msg, const DataType& data) {
+        static_assert (std::is_standard_layout<DataType>::value, "data is too complex to pushed into the vector.");
+        static_assert (std::is_trivial<DataType>::value, "data is too complex to pushed into the vector.");
         size_t i = msg.body.size();
-
-        // resize the vector by the sizeof the data being pushed.
         msg.body.resize(msg.body.size() + sizeof (DataType));
-
-        // physically copy the data into the newly allocated vector space.
         std::memcpy(msg.body.data() + i, &data, sizeof (DataType));
-
-        // reallocate the message size.
         msg.header.size = msg.size();
 
         return msg;
     }
 
-    template <typename DataType>
-    friend message<T>& operator>>(message<T>& msg, DataType& data) {
-
-        static_assert (std::is_standard_layout<DataType>::value, "Data is too complex - not standard layout.");
-        static_assert (std::is_trivial<DataType>::value, "Data is not trivial.");
-
-        // cache the location towards the end of the vector where the pulled data starts.
-
-        // shrink the vector to remove read bytes, and reset end position.
-
-        // realocate the message size
-
+    template<typename DataType>
+    friend message<T>& operator >> (message<T>& msg, DataType& data) {
+        static_assert (std::is_standard_layout<DataType>::value, "data is too complex to pushed into the vector.");
+        static_assert (std::is_trivial<DataType>::value, "data is too complex to pushed into the vector.");
+        size_t i = msg.body.size() - sizeof (DataType);
+        std::memcpy(&data, msg.body.data() + i, sizeof (DataType));
+        msg.body.resize(i);
+        msg.header.size = msg.size();
 
         return msg;
     }
-
-
 };
-
-
-
-
-
 }
-
 
 #endif // MESSAGE_H
